0f1a95af0d8cd248b702df87735dc6b26c31e0ae
moritzgubler <moritz.gubler@gmail.com>
2022-07-13 12:32:50 +0200

    set filename to testfile in test directory.

--- src/main.cpp
+++ src/main.cpp
@@ -58,8 +58,9 @@ void read_ascii(string &fname, int &nat, MatrixXd &r, Vector3d &lat_a, Vector3d 
 int main(int argc, char **argv) {
 
   int nat = 8;
-  string fname = argv[1];
-  cout << fname << endl;
+  //string fname = argv[1];
+  //cout << fname << endl;
+  string fname = "../test/test.ascii";
   MatrixXd r(3, nat);
   MatrixXd f(3, nat);
   Vector3d lat_a, lat_b, lat_c;
