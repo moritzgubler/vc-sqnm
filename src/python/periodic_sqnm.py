8e74fbc422a03dd673c82e2cb2167a81eabf445c
moritzgubler <moritz.gubler@gmail.com>
2022-08-30 16:22:48 +0200

    fixed some bugs in coordinate transformation

--- src/python/periodic_sqnm.py
+++ src/python/periodic_sqnm.py
@@ -1,4 +1,3 @@
-from mimetypes import init
 import numpy as np
 import sqnm
 
@@ -21,7 +20,7 @@ class periodic_sqnm:
         a_inv = np.linalg.inv(alat)
 
         q = ((self.initial_lat @ a_inv) @ pos).reshape(3 * self.nat)
-        df_dq = (- (alat @ self.initial_lat) @ forces).reshape(3 * self.nat)
+        df_dq = (- (alat @ self.initial_lat_inverse) @ forces).reshape(3 * self.nat)
 
         a_tilde = (alat @ self.lattice_transformer).reshape(9)
         df_da_tilde = (- deralat @ self.lattice_transformer).reshape(9)
@@ -33,12 +32,35 @@ class periodic_sqnm:
 
         q_and_lat = q_and_lat + dd
 
-        q = q_and_lat[:(3 * self.nat)]
-        a_tilde = q_and_lat[(3*self.nat):]
+        q = q_and_lat[:(3 * self.nat)].reshape(3, self.nat)
+        a_tilde = q_and_lat[(3*self.nat):].reshape(3, 3)
 
         alat = a_tilde @ self.lattice_transformer_inv
         pos = (alat @ self.initial_lat_inverse) @ q
 
         return pos, alat
+
+"""
+def energyandforces(nat, pos, alat):
+    epot, forces, deralat = bazant.energyandforces_bazant(alat, pos, nat)
+    return epot, forces, deralat
+
+
+b2a = Bohr_Ang = 0.52917721067
+at = io.read('../../test/test.ascii')
+pos = at.get_positions().T / b2a
+lat = at.get_cell().T / b2a
+nat = 8
+alpha = 1
+
+
+opt = periodic_sqnm(nat, lat, alpha, 10, 2.0, 1e-2, 1e-4)
+
+for i in range(390):
+    epot, forces, deralat = energyandforces(nat, pos, lat)
+    print(epot, np.linalg.norm(forces), np.linalg.norm(deralat))
+    pos, lat = opt.optimizer_step(pos, lat, epot, forces, deralat)
+"""
+
 
 
