89deea68c2e8db617a3992f512b38e864778e1d2
moritzgubler <moritz.gubler@gmail.com>
2022-08-30 12:00:22 +0200

    added tests, passes tests

--- src/python/sqnm.py
+++ src/python/sqnm.py
@@ -1,15 +1,16 @@
 import numpy as np
 import historylist
+import time
 
 class SQNM:
-    def __init__(self, ndim, nhist_max, eps_supsp, alpha_min):
+    def __init__(self, ndim, nhist_max, alpha, eps_supsp, alpha_min):
         self.ndim = ndim
         self.nhist_max = nhist_max
         self.eps_subsp = eps_supsp
         self.alpha_min = alpha_min
         self.xlist = historylist.HistoryList(self.ndim, self.nhist_max)
         self.flist = historylist.HistoryList(self.ndim, self.nhist_max)
-        self.alpha = 0.0
+        self.alpha = alpha
         self.dir_of_descent = np.zeros(ndim)
         self.prev_f_of_x = 0.0
         self.prev_df_dx = np.zeros(ndim)
@@ -43,8 +44,8 @@ class SQNM:
             # calculate overlap matrix of basis
             self.s_evec[:self.nhist, :self.nhist] = self.xlist.normalizedDiffList[:, :self.nhist].T \
                 @ self.xlist.normalizedDiffList[:, :self.nhist]
-            self.s_eval[:self.nhist], self.s_evec[:self.nhist, self.nhist] \
-                = np.linal.eigh(self.s_evec[:self.nhist, self.nhist])
+            self.s_eval[:self.nhist], self.s_evec[:self.nhist, :self.nhist] \
+                = np.linalg.eigh(self.s_evec[:self.nhist, :self.nhist])
 
             # remove noisy directions from subspace
             dim_subsp = 0
@@ -52,8 +53,8 @@ class SQNM:
                 if self.s_eval[i] / self.s_eval[self.nhist - 1] > self.eps_subsp:
                     dim_subsp += 1
 
-            self.s_eval[:dim_subsp] = self.s_eval[(self.nhist - dim_subsp):]
-            self.s_evec[:, :dim_subsp] = self.s_evec[:, (self.nhist - dim_subsp):]
+            self.s_eval[:dim_subsp] = self.s_eval[(self.nhist - dim_subsp):self.nhist]
+            self.s_evec[:, :dim_subsp] = self.s_evec[:, (self.nhist - dim_subsp):self.nhist]
 
             # compute eq. 11
             self.dr_subsp[:, :] = 0.0
@@ -83,7 +84,7 @@ class SQNM:
                 self.res_temp = - self.h_eval[j] * self.h_evec[:, j]
                 for k in range(dim_subsp):
                     self.res_temp += self.h_evec_subsp[k, j] * self.df_subsp[:, k]
-                self.res[j] = np.linalg(self.res_temp)
+                self.res[j] = np.linalg.norm(self.res_temp)
 
             # modify eigenvalues according to eq. 18
             self.h_eval[:dim_subsp] = np.sqrt(self.h_eval[:dim_subsp]**2 + self.res[:dim_subsp]**2)
@@ -91,12 +92,13 @@ class SQNM:
             # decompose gradient according to eq. 16
             self.dir_of_descent = df_dx
             for i in range(dim_subsp):
-                self.dir_of_descent -= np.dot(self.h_evec[:, i], df_dx) * self.h_evec[:, i]
+                self.dir_of_descent = self.dir_of_descent - np.dot(self.h_evec[:, i], df_dx) * self.h_evec[:, i]
             self.dir_of_descent = self.dir_of_descent * self.alpha
 
             # apply preconditioning to subspace gradient (eq. 21)
             for idim in range(dim_subsp):
-                self.dir_of_descent += np.dot( df_dx, self.h_evec[:, idim] ) / self.h_eval[idim] * self.h_evec[:, idim]
+                self.dir_of_descent = self.dir_of_descent + np.dot( df_dx, self.h_evec[:, idim] ) * self.h_evec[:, idim] / self.h_eval[idim]
+            #print('dd2', self.dir_of_descent)
 
             self.dir_of_descent = - self.dir_of_descent
         self.prev_f_of_x = f_of_x
@@ -104,3 +106,39 @@ class SQNM:
         return self.dir_of_descent
 
 
+"""
+def test_fun(x):
+    n = len(x)
+    a = np.zeros((n, n))
+    for i in range(n):
+        a[i, i] = .5*i + 1
+    f = .5 * x.T @ a @ x
+    df = a @ x
+    return f, df
+
+
+
+n = 400
+nhistx = 20
+alpha = .1
+x = np.zeros(n)
+x[:] = 1.0
+x[1] = 1.4
+
+opt = SQNM(n, nhistx, alpha, 1e-4, 1e-2)
+
+
+for i in range(40):
+    f, df = test_fun(x)
+    print(i)
+    print('norm of x', np.linalg.norm(x))
+    print(f)
+    print('norm of forces', np.linalg.norm(df))
+    print('')
+    t1 = time.time()
+    dd = opt.sqnm_step(x, f, df)
+    t2 = time.time()
+    print('time', t2 -t1)
+    # print('dd', dd)
+    x += dd
+"""
