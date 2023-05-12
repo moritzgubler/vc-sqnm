f65c1585f3967e7a3a6c5cc53412655ce12f4721
moritzgubler <moritz.gubler@gmail.com>
2023-05-12 16:58:05 +0200

    added counter

--- src/python/sqnm/vcsqnm_for_ase.py
+++ src/python/sqnm/vcsqnm_for_ase.py
@@ -92,6 +92,7 @@ class aseOptimizer():
         while( i < self.maximalSteps and self._getDerivativeNorm() > self.force_tol ):
             logging.info("Relaxation step: %d energy: %f norm of forces: %f"% (i, self.initial_structure.get_potential_energy(), np.max(np.abs(self.initial_structure.get_forces()))) )
             self.step(self.initial_structure)
+            i += 1
         return self.initial_structure
 
         
